790ad9b4038d7fe4baa17dda357ff7848df360e8
Kilian Agboton <agbotonstefane@gmail.com>
2024-06-17 12:46:14 -0400

    created card component to display card information
    created Card interface
    added data inside app component
    sent data from app component(parent) to card component (child) with @Input
    bind properties inside card template file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,35 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CardComponent } from './card/card.component';
+import { Card } from './card/Card';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CardComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'cards';
+ 
+  posts: Card[] = [
+    {
+      title: 'Neat Tree',
+      imageUrl: 'assets/tree.jpeg',
+      username: 'nature',
+      content: 'I saw this neat tree today',
+    },
+    {
+      title: 'Snow Mountain',
+      imageUrl: 'assets/mountain.jpeg',
+      username: 'mountainlover',
+      content: 'Here is a picture of a snow mountain!',
+    },
+    {
+      title: 'Biking',
+      imageUrl: 'assets/biking.jpeg',
+      username: 'biking1239',
+      content: 'I did some biking today',
+    },
+  ];
 }
